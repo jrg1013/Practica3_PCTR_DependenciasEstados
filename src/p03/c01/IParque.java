7d55368c0133485f19adb5d1fe403c5061f9d406
rmt0009alu <rodmertov@gmail.com>
2022-03-30 14:06:14 +0200

    Documentación orientativa inicial #4
    Documentar, brevemente, la interfaz

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,10 +1,31 @@
 package src.p03.c01;
 
+/**
+ * Interfaz que representa las dos funciones principales del objeto pasivo
+ * y recurso compartido Parque. 
+ * 
+ * @author Carlos López Nozal
+ * @author José Antonio Barbero Aparicio
+ * @author José Redondo Guerra
+ * @author Rodrigo Merino Tovar
+ * 
+ * @version 1.0
+ * @since 1.0
+ */
 public interface IParque {
 	
+	/**
+	 * Método abstracto para indicar que se entra por una puerta. 
+	 * 
+	 * @param puerta Puerta por la que se entra.
+	 */
 	public abstract void entrarAlParque(String puerta);
 
-	// Añadimos la nueva opción de salir del parque
+	/**
+	 * Método abstracto para indicar que se sale por una puerta. 
+	 * 
+	 * @param puerta Puerta por la que se sale.
+	 */
 	public abstract void salirDelParque(String puerta);
 
 }
